3309830068532f5a0eeb59cd8809bb09ab503a14
Peter Noronha <petedomain@gmail.com>
2022-04-13 23:36:52 +0400

    API body parser, server logs and CORS headers

--- server/app.js
+++ server/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const downloadRoutes = require('./api/routes/downloads');
 
+// CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.use('/downloads', downloadRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
