465a25ecbdf30e3d6cc0b9d566da75ed420d3abc
Peter Noronha <petedomain@gmail.com>
2022-04-13 23:46:19 +0400

    API header options

--- server/app.js
+++ server/app.js
@@ -7,7 +7,15 @@ const downloadRoutes = require('./api/routes/downloads');
 // CORS headers
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+        return res.status(200).json({});
+    }
+    next();
 });
 
 app.use(morgan('dev'));
